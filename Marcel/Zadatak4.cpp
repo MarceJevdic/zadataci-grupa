5cc04acad803d4394cc09fd9543928efa9f9ba2b
MarceJevdic <marcel.jevdic@gmail.com>
2025-11-03 16:52:39 +0100

    Add files via upload

---
+++ Marcel/Zadatak4.cpp
@@ -0,0 +1,40 @@
+﻿#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+using namespace std;
+
+/*Napišite funkciju koja provjerava je li jedna rečenica palindrom (ignoriraju se praznine
+i znakovi interpunkcije).
+*/
+
+void funkcija(string& recenica) {
+	string novi;
+	
+	auto it = remove_if(recenica.begin(), recenica.end(), [](char c) {
+		return (ispunct(c) || c == ' ');
+		});
+
+	recenica.erase(it, recenica.end());
+	novi = recenica;
+
+	reverse(novi.begin(), novi.end());
+
+	if (novi == recenica) {
+		cout << "palindrom je";
+	}
+	else {
+		cout << "nije palindrom";
+	}
+	
+
+
+}
+
+
+int main() {
+	string recenica = "Marko . okraM";
+	funkcija(recenica);
+
+	return 0;
+}
