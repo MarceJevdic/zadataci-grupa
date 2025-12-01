7a52170f167e4412bb6e73c93c81114272c1dc54
dujereic05 <dududuje@gmail.com>
2025-12-01 18:48:41 +0100

    Refactor Patuljak and MinArray classes

--- DUJE/patuljci.cpp
+++ DUJE/patuljci.cpp
@@ -1,47 +1,57 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <cmath>
 #include <algorithm>
 using namespace std;
+
 class Patuljak{
-    private:
-        string ime;
-        int brzina;
-    public:
-        Patuljak(string ime, int brzina){
-            this->ime=ime;
-            this->brzina=brzina;
-        }
-        int get_brzina() const{
-            return brzina;
-        }
+private:
+    string ime;
+    int brzina;
+public:
+    Patuljak(string ime, int brzina){
+        this->ime=ime;
+        this->brzina=brzina;
+    }
+    int get_brzina() const{
+        return brzina;
+    }
+    void set_brzina(int nova){
+        brzina = nova;
+    }
 };
+
 class MinArray{
-    private:
-        vector<int> brzine;
-    public:
-        void push(int value){
-            brzine.push_back(value);
-        }
-        int front() const{
-            int najmanji=brzine[0];
-            for(int i=0;i<brzine.size();i++){
-                if(najmanji>brzine[i]){
-                    najmanji=brzine[i];
-                }
-            }
-            return najmanji;
-        }
-        void minify(){
-            int temp=0;
-            for(int i=0;i<brzine.size();i++){
-                if(brzine[0]>brzine[i]){
-                    swap(brzine[0],brzine[i]);
-                }
+private:
+    vector<int> brzine;
+public:
+    void push(int value){
+        brzine.push_back(value);
+    }
+    int front() const{
+        int najmanji=brzine[0];
+        for(int i=0;i<brzine.size();i++){
+            if(najmanji>brzine[i]){
+                najmanji=brzine[i];
             }
         }
+        return najmanji;
+    }
+    void minify(){
+        for(int i=0;i<brzine.size();i++){
+            if(brzine[0]>brzine[i]){
+                swap(brzine[0],brzine[i]);
+            }
+        }
+    }
+    size_t size() const{
+        return brzine.size();
+    }
+    vector<int>& get_vector(){
+        return brzine;
+    }
 };
+
 int main() {
     vector<Patuljak> patuljci = {
         {"Srecko", 6}, {"Ljutko", 4}, {"Stidljivko", 3},
@@ -54,8 +64,15 @@ int main() {
         arr.push(p.get_brzina());
     }
 
-    arr.minify(); // stavi najmanju brzinu na početak
-    cout << "Najmanja brzina nakon minify: " << arr.front() << endl;
+    for(int krug=1; krug<=3; krug++){
+        arr.minify();
+        cout << "Krug " << krug << ": Najsporija brzina = " << arr.front() << endl;
+        int nova;
+        cout << "Unesite novu brzinu najsporijeg patuljka: ";
+        cin >> nova;
+        arr.get_vector()[0] = nova;
+    }
 
     return 0;
 }
+
